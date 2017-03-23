68093d270f4e8daf9a0095efc81753bada331d15
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-03-23 15:55:54 +0100

    Use 64-bit registers when referring to memory

--- dijkstra_semaphore.asm
+++ dijkstra_semaphore.asm
@@ -5,17 +5,17 @@ section .text
 
 ;-------------------------------------------------------------------------------
 ; proberen -- attempt to grab the semaphore
-;        IN: EDI = pointer to 32-bit integer, the semaphore's value
+;        IN: RDI = pointer to 32-bit integer, the semaphore's value
 ; CLOBBERED: RAX
 ;            All other registers preserved
 spin_lock:
-  lock inc dword [edi]
+  lock inc dword [rdi]
 proberen:
-  mov eax, [edi]
+  mov eax, [rdi]
   test eax, eax        ; check if semaphore > 0
   jle proberen         ; if not, spinlock
   mov eax, -1
-  lock xadd [edi], eax ; try decreasing the semaphore
+  lock xadd [rdi], eax ; try decreasing the semaphore
   cmp eax, 0           ; check if the semaphore was > 0
   jle spin_lock        ; if not, spinlock, adding back the 1
   ret
@@ -25,5 +25,5 @@ proberen:
 ;  IN: edi = pointer to 32-bit integer, the semaphore's value
 ;      All registers preserved
 verhogen:
-  lock inc dword [edi]
+  lock inc dword [rdi]
   ret
